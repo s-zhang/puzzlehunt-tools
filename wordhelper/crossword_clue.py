b4a40205c9b76fd8f2dc6602f856166f8412be7b
Gordon Zhou <gordonzhoudev@gmail.com>
2018-05-13 00:56:06 -0700

    Add ability to not require a pattern

--- wordhelper/crossword_clue.py
+++ wordhelper/crossword_clue.py
@@ -2,14 +2,17 @@ import requests
 import urllib.parse
 from bs4 import BeautifulSoup
 
-def crossword_clue(clue, pattern):
+def crossword_clue(clue, pattern = ''):
     encoded_clue = urllib.parse.quote_plus(clue)
     encoded_pattern = urllib.parse.quote_plus(pattern)
     pattern_length = len(pattern)
 
+    if pattern_length == 0:
+        pattern_length = 'any'
+
     response = requests.get(f'http://www.dictionary.com/fun/crosswordsolver?query={encoded_clue}&pattern={encoded_pattern}&l={pattern_length}')
     soup = BeautifulSoup(response.text, 'html.parser')
-    resultElements = soup.find_all("div", {'class': 'matching-answer'})
+    resultElements = soup.find_all('div', {'class': 'matching-answer'})
     results = []
     for resultElement in resultElements[1:]:
         results.append(resultElement.text.strip())
