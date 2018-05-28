ba603cd00df5ae522b8960b6f2583bcb81ad702c
Gordon Zhou <gordonzhoudev@gmail.com>
2018-05-28 02:04:51 -0700

    Skip crossword clue tests due to test runner connectivity

--- puzzle-utils/words/tests/crossword_clue_test.py
+++ puzzle-utils/words/tests/crossword_clue_test.py
@@ -1,7 +1,10 @@
+import pytest
 from ..crossword_clue import crossword_clue, crossword_clue_multi
 
+@pytest.mark.skip(reason = 'Automated test runners cannot talk to the internet')
 def test_crossword_clue():
 	assert crossword_clue('largest sea creature', 'WH???') == ['WHALE', 'WHELK']
 
+@pytest.mark.skip(reason = 'Automated test runners cannot talk to the internet')
 def test_crossword_clue_multi():
 	assert crossword_clue_multi(['largest sea creature', 'the great white north'], ['WH???', '???ADA']) == [['WHALE', 'WHELK'], ['CANADA', 'MASADA']]
