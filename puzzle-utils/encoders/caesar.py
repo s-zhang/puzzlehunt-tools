27824a71c4a51d7a5a884fa44cafce8c76c38d0a
Nicolas Arciniega <nick7650@hotmail.com>
2018-07-17 23:00:19 -0700

    Use google docstring standard and type hints

--- puzzle-utils/encoders/caesar.py
+++ puzzle-utils/encoders/caesar.py
@@ -1,8 +1,11 @@
+from typing import List
+
 # All input should be uppercased
 CHARACTER_OFFSET = 65
 ALPHABET_SIZE = 26
 
-def shift_letter(letter, shift_count):
+def shift_letter(letter: str, shift_count: int) -> int:
+    """Shift a letter by some number"""
     if not isinstance(letter, str) or not letter.isalpha():
         return letter
 
@@ -13,50 +16,50 @@ def shift_letter(letter, shift_count):
     char_code = (char_code + shift_count) % ALPHABET_SIZE
     return chr(char_code + CHARACTER_OFFSET)
 
-def caesar(word, shift_count):
-    """
-    Caesar shift a whole word by a number
+def caesar(word: str, shift_count: int) -> str:
+    """ Caesar shift a whole word by a number
 
-    Params:
-    word        (required) - The word to shift
-    shift_count (required) - The count to shift by
+    Args:
+        word: The word to shift
+        shift_count: The count to shift by
 
-    Alternatives:
-    brute_caesar       - Shift a whole word by 1 - 25
-    independent_caesar - Shift a word by different numbers for each letter
+    See Also:
+        caesar_brute: Shift a whole word by 1 - 25
+        caesar_independent: Shift a word by different numbers for each letter
     """
     new_word = ''
     for letter in word.upper():
         new_word += shift_letter(letter, shift_count)
     return new_word
 
-def brute_caesar(word):
-    """
-    Caesar shift a whole word by 1 - 25
+def caesar_brute(word: str) -> List[str]:
+    """ Caesar shift a whole word by 1 - 25
 
-    Params:
-    word        (required) - The word to shift
+    Args:
+        word: The word to shift
 
-    Alternatives:
-    caesar             - Shift a whole word by a number
-    independent_caesar - Shift a word by different numbers for each letter
+    See Also:
+        caesar: Shift a whole word by a number
+        caesar_independent: Shift a word by different numbers for each letter
     """
     shifted_words = []
     for shift_by in range(1, ALPHABET_SIZE):
         shifted_words.append(caesar(word, shift_by))
     return shifted_words
 
-def independent_caesar(letters, shift_counts):
-    """
-    Caesar shift a whole word by a number
+def caesar_independent(letters: str, shift_counts: List[int]) -> str:
+    """Caesar shift a whole word by a number
 
-    Params:
-    letters      (required) - The word to shift
-    shift_counts (required) - A list of counts to shift by
+    Args:
+        letters: The word to shift
+        shift_counts: A list of counts to shift by
 
-    Alternatives:
-    caesar       - Shift a whole word by a number
-    brute_caesar - Shift a whole word by 1 - 25
+    Raises:
+        ValueError: The length of shift_counts must be equal to the number of letters
+
+    See Also:
+        caesar: Shift a whole word by a number
+        caesar_brute: Shift a whole word by 1 - 25
     """
     if len(letters) != len(shift_counts):
         raise ValueError("Word length must be the same as shift counts length")
