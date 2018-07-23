355b3b284118eefee3ede2482384ef16826c6747
Gordon Zhou <gordonzhoudev@gmail.com>
2018-07-22 21:38:18 -0700

    Add support for irregular grids and strings in each cell

--- puzzle-utils/words/wordsearch.py
+++ puzzle-utils/words/wordsearch.py
@@ -23,25 +23,32 @@ def wordsearch_reduce(grid, words):
     """
     results = wordsearch_find(grid, words)
 
+    # Create a normalized grid for reference and another for keeping track of used cells
+    reference_grid = []
     working_grid = []
     for row in grid:
-        working_grid.append(list(row))
+        reference_row = [x.lower() for x in list(row)]
+        reference_grid.append(reference_row)
+        working_grid.append(reference_row[:])
 
+    # Remove words from the working grid
     for result in results:
         row = result.row
         column = result.column
         row_increment, column_increment = direction_to_increment_mapping[result.direction]
-        for _ in result.word:
+        while len(result.word) > 0:
+            result.word = result.word.replace(reference_grid[row][column], '', 1)
             working_grid[row][column] = None
             row += row_increment
             column += column_increment
     
+    # Compress the working grid
     reduced_grid = []
     for row in range(len(working_grid)):
         reduced_grid.append('')
-        for char in working_grid[row]:
-            if char != None:
-                reduced_grid[row] += char
+        for cell in working_grid[row]:
+            if cell != None and not cell.isspace():
+                reduced_grid[row] += cell
     
     return reduced_grid
 
@@ -79,13 +86,28 @@ def wordsearch_find(grid, words):
     for word in words:
         normalized_words.append(word.lower())
 
-    return wordsearch_grid(grid, wordsearch_find_function, normalized_words)
+    results = wordsearch_grid(grid, wordsearch_find_function, normalized_words)
+
+    missing_words = []
+    for word in words:
+        found = False
+        for result in results:
+            if result.word == word:
+                found = True
+                break
+        if not found:
+            missing_words.append(word)
+    
+    if len(missing_words) > 0:
+        raise ValueError(f'The following word(s) were not found: {", ".join(missing_words)}')
+
+    return results
 
 def wordsearch_find_function(word, row, column, direction, words):
     if word in words:
         return WordSearchResult(word, row, column, direction)
 
-def wordsearch_brute(grid, min_length):
+def wordsearch_brute(grid, min_length = 4):
     """
         Finds all words and returns their location and direction in the grid.
         Example:
@@ -96,7 +118,7 @@ def wordsearch_brute(grid, min_length):
                 'RLDN'
             ]
         >>> min_length = 4
-        >>> wordsearch_find(grid, min_length)
+        >>> wordsearch_brute(grid, min_length)
     """
     return wordsearch_grid(grid, wordsearch_brute_function, min_length)
 
@@ -105,10 +127,14 @@ def wordsearch_brute_function(word, row, column, direction, min_length):
         return WordSearchResult(word, row, column, direction)
 
 def wordsearch_grid(grid, function, parameters):
+    normalized_grid = []
+    for row in grid:
+        normalized_grid.append([x.lower() for x in list(row)])
+
     results = []
-    for row in range(len(grid)):
-        for column in range(len(grid[row])):
-            results += wordsearch_point(grid, row, column, function, parameters)
+    for row in range(len(normalized_grid)):
+        for column in range(len(normalized_grid[row])):
+            results += wordsearch_point(normalized_grid, row, column, function, parameters)
 
     return results
 
@@ -126,7 +152,7 @@ def wordsearch_point_direction(grid, row, column, direction, function, parameter
     column_current = column
     row_increment, column_increment = direction_to_increment_mapping[direction]
     while row_current > -1 and row_current < len(grid) and column_current > -1 and column_current < len(grid[row_current]):
-        word += grid[row_current][column_current].lower()
+        word += grid[row_current][column_current]
         row_current += row_increment
         column_current += column_increment
         result = function(word, row, column, direction, parameters)
